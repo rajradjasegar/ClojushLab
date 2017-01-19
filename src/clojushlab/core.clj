0f4d7e6c5302d1452f5c6e8a79eaa90be17cc6b2
Youssouf Sissoko <ysissoko78@gmail.com>
2017-01-19 01:49:29 +0100

    Creation de la grammaire

--- src/clojushlab/core.clj
+++ src/clojushlab/core.clj
@@ -1,3 +1,36 @@
-(ns clojushlab.core)
+(ns clojushlab.core
+  (:require [instaparse.core :as insta]))
 
-(println "This is our project Clojush Lab !")
+(def vec-parser
+  (insta/parser
+    "instrs = ((assign? | expr?) <';'> <space>?)*
+     
+     (* expression *)
+     expr = (<space>? (assign | operation) <space>?)*
+     
+     (* Affectation *)
+     assign = var <space>? <'='> <space>? (operation | number)  
+     
+     (* variables *)
+     var = #'[a-zA-Z0-9]+'
+     
+     (* operations *)
+     operation = matrix? <space>? op <space>? matrix?
+
+     (* operateurs *)
+     op = '*' | '+' | '-'
+
+     (* matrices *)
+     matrix = <'m('> <space>? matrow* <space>? <')'>
+
+     (* Represente une ligne de la matrice *)
+     matrow = <'['> <space>? (number <space>)*  <']'>
+
+     (* Les espaces *)
+     <space> = #'[\\s]*'
+
+     (* Les constantes de 0 à 9 *)
+     number = #'[0-9]+'"
+  ))
+
+(vec-parser "m([5454 151 484 4]) * m([5454 151 484 4]); v = m([1 2 5 8 7]) + m([1]);")

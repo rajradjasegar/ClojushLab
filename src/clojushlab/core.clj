57fe087ab903eebc644100b1ed30b81c8b7ecb27
Oclean14 <Oclean14@users.noreply.github.com>
2017-01-26 05:00:09 +0100

    puissance

--- src/clojushlab/core.clj
+++ src/clojushlab/core.clj
@@ -17,8 +17,8 @@
      (* Termes de l'operation *)  
      <elt> = number | negnum | operation | varget | matrix
 
-     (* operateurs *)
-     op = '*' | '+' | '-'
+	(* operateurs *)
+     op = '*' | '+' | '-' | '^'
 
      (* matrices *)
      matrix = <'m['> (space? matrow space?)* <']'>
@@ -38,7 +38,8 @@
      (* variables *)
      varname = #'[a-zA-Z]\\w*'"
   ))
-
+;(* operations *) est ce qu'on peut l'ajouter ?
+;operation = matrix <space>? op <space>? number
 
 (defn add [op el1 el2] 
 (mapv #(mapv op %1 %2) el1 el2))
@@ -53,10 +54,19 @@
          (map (fn [b] 
                 (f a b)) y))
        x))
-
 (defn mul
   [el1 el2]
   (nested-for (fn [x y] (reduce + (map * x y))) el1 (transpose el2)))
+(defn mat-power [m1 power] 
+  (def b m1)
+ (loop [x power]
+ (when (> x 1)
+   (def b (mul m1 b))
+ 
+   (recur (- x 1))))
+(println b)
+)
+
 
 (defn execute 
   [el1 op el2]
@@ -64,6 +74,7 @@
         [:op "*"] (mul el1 el2)
         [:op "+"] (add + el1 el2)
         [:op "-"] (add - el1 el2)
+        [:op "^"] (mat-power el1 el2 )
         "Unknown operator"))
 
 (def matrice-interpret
@@ -75,5 +86,8 @@
    :operation execute
    })
 
-(insta/transform matrice-interpret (matrice-parser "(m[ {1 2 3} {4 +5 6}] + m[ {7 8 9} {10 -11 12}]) * m[ {1 8 9} {1 2 6} {2 4 5}];"))
+;(insta/transform matrice-interpret (matrice-parser "(m[ {1 2 3} {4 +5 6}] + m[ {7 8 9} {10 -11 12}]) * m[ {1 8 9} {1 2 6} {2 4 5}];"))
 ;(matrice-parser "m[ {7 2 3} {4 5 6}] + m[ {7 8 9} {10 11 12} ]  - m[ {13  14 15} {16 17 18}]  ];")
+
+(insta/transform matrice-interpret 
+               (matrice-parser " ((m[{0 0} {1 0}] + m[{2 1} {0 2}]) ^ 2)  ;"))
